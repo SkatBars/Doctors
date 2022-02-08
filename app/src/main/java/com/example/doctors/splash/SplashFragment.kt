d2e435d71b7ef7b53d350486a80fb3563f7e589b
1_skat_1 <skat.apps@gmail.com>
2022-02-08 19:55:57 +0300

    add binding in SplashFragment.kt

--- app/src/main/java/com/example/doctors/splash/SplashFragment.kt
+++ app/src/main/java/com/example/doctors/splash/SplashFragment.kt
@@ -7,16 +7,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.doctors.R
+import com.example.doctors.databinding.FragmentSplashBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashFragment : Fragment() {
 
     private val viewModel: SplashViewModel by viewModel()
+    private lateinit var binding: FragmentSplashBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(inflater)
+        binding.startBtn.setOnClickListener {
+
+        }
+        return binding.root
     }
 }
