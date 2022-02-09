554768904e4a7b45b911052dc6f5f4c7c324a653
1_skat_1 <skat.apps@gmail.com>
2022-02-09 23:27:18 +0300

    add data binding in SignInFragment.kt and add method showGoogleAuthorizationActivity()

--- app/src/main/java/com/example/doctors/autorization/data/signIn/SignInFragment.kt
+++ app/src/main/java/com/example/doctors/autorization/data/signIn/SignInFragment.kt
@@ -6,19 +6,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.doctors.R
+import com.example.doctors.RC_SIGN_IN
+import com.example.doctors.databinding.FragmentSignInBinding
+import com.example.doctors.databinding.FragmentSplashBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignInFragment : Fragment() {
 
     private val viewModel: SignInViewModel by viewModel()
+    private lateinit var binding: FragmentSignInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        binding = FragmentSignInBinding.inflate(inflater)
+
+        binding.signInGoogleBtn.setOnClickListener {
+            showGoogleAuthorizationActivity()
+        }
+
+        return binding.root
     }
 
+    private fun showGoogleAuthorizationActivity() {
+        val googleSignInClient = GoogleSignIn.getClient(requireActivity(), viewModel.gsoDb)
+        val signInGoogleIntent = googleSignInClient.signInIntent
+        requireActivity().startActivityIfNeeded(signInGoogleIntent, RC_SIGN_IN)
+    }
 
 }
