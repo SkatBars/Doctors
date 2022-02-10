08d0e9fe037f28e6ea9e08a1beaed9dde7a5db2b
1_skat_1 <skat.apps@gmail.com>
2022-02-11 02:11:12 +0300

    set clickListener on registrationBtn

--- app/src/main/java/com/example/doctors/authorization/view/SignInFragment.kt
+++ app/src/main/java/com/example/doctors/authorization/view/SignInFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.doctors.R
 import com.example.doctors.RC_SIGN_IN
 import com.example.doctors.authorization.viewModel.AuthorizationViewModel
 import com.example.doctors.databinding.FragmentSignInBinding
@@ -51,6 +53,9 @@ class SignInFragment : Fragment() {
                 val password = passwordEditTextSignIn.text.toString()
                 viewModel.signInWithEmail(email, password)
             }
+            registrationBtn.setOnClickListener {
+                findNavController()
+                    .navigate(R.id.action_signInFragment_to_registrationFragment) }
         }
     }
 
