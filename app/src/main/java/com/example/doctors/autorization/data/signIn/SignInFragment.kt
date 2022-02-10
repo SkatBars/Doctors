496ea991b3a3682d97477049d2dd962410dbae3e
1_skat_1 <skat.apps@gmail.com>
2022-02-11 01:14:15 +0300

    refactoring onActivityResult

--- app/src/main/java/com/example/doctors/autorization/data/signIn/SignInFragment.kt
+++ app/src/main/java/com/example/doctors/autorization/data/signIn/SignInFragment.kt
@@ -1,19 +1,15 @@
 package com.example.doctors.autorization.data.signIn
 
 import android.content.Intent
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.databinding.DataBindingUtil
-import com.example.doctors.R
 import com.example.doctors.RC_SIGN_IN
 import com.example.doctors.databinding.FragmentSignInBinding
-import com.example.doctors.databinding.FragmentSplashBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -35,19 +31,28 @@ class SignInFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         if (requestCode == RC_SIGN_IN) {
-            viewModel.logInWithGoogle()
-            Snackbar.make(binding.root, "yes", Snackbar.LENGTH_LONG).show()
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                viewModel.signInWithGoogle(account.idToken!!)
+            }catch (e: ApiException) {
+                showSnackbar(e.message.toString())
+            }
         }
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE).show()
     }
 
     private fun showGoogleAuthorizationActivity() {
         val googleSignInClient = GoogleSignIn.getClient(requireActivity(), viewModel.gsoDb)
         val signInGoogleIntent = googleSignInClient.signInIntent
-        requireActivity().startActivityIfNeeded(signInGoogleIntent, RC_SIGN_IN)
+        startActivityForResult(signInGoogleIntent, RC_SIGN_IN)
     }
 
 }
