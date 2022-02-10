90325762f491c24a27b1b99ec962cc55fbf8a387
1_skat_1 <skat.apps@gmail.com>
2022-02-11 02:44:00 +0300

    create mainFragment

--- app/src/main/java/com/example/doctors/di/modules.kt
+++ app/src/main/java/com/example/doctors/di/modules.kt
@@ -2,6 +2,7 @@ package com.example.doctors.di
 
 import com.example.doctors.authorization.data.FirebaseAuthDataSource
 import com.example.doctors.authorization.viewModel.AuthorizationViewModel
+import com.example.doctors.main.MainViewModel
 import com.example.doctors.splash.SplashViewModel
 import com.google.firebase.auth.FirebaseAuth
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -14,4 +15,5 @@ val modules = module {
 
     viewModel { SplashViewModel() }
     viewModel { AuthorizationViewModel(db = get()) }
+    viewModel { MainViewModel(db = get()) }
 }
---
+++ app/src/main/java/com/example/doctors/main/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.example.doctors.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.doctors.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
