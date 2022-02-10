63a949a6c5b57350aae0d7ed53c0432166623e8c
1_skat_1 <skat.apps@gmail.com>
2022-02-11 02:33:14 +0300

    create methods in RegistrationFragment.kt: * configurationClickListeners * configurationLiveDataObservers

--- app/src/main/java/com/example/doctors/authorization/view/RegistrationFragment.kt
+++ app/src/main/java/com/example/doctors/authorization/view/RegistrationFragment.kt
@@ -5,18 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.doctors.R
 import com.example.doctors.authorization.viewModel.AuthorizationViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.example.doctors.databinding.FragmentRegistrationBinding
+import org.koin.android.ext.android.bind
 
 class RegistrationFragment : Fragment() {
 
     private val viewModel: AuthorizationViewModel by viewModel()
+    private lateinit var binding: FragmentRegistrationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        binding = FragmentRegistrationBinding.inflate(inflater)
+        configurationClickListeners()
+        configurationLiveDataObservers()
+
+        return binding.root
+    }
+
+    private fun configurationLiveDataObservers() {
+
+    }
+
+    private fun configurationClickListeners() {
+        with(binding) {
+            registrationBtn.setOnClickListener {
+                val email = emailEditTextRegistration.text.toString()
+                val password = passwordEditTextRegistration.text.toString()
+                val repeatPassword = repeatPasswordEditTextRegistration.text.toString()
+
+                viewModel.register(email, password, repeatPassword)}
+            backBtn.setOnClickListener {findNavController().popBackStack()}
+        }
     }
 }
