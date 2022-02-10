05f170ebc31280a68acb880effe361e9b03df00d
1_skat_1 <skat.apps@gmail.com>
2022-02-10 23:58:38 +0300

    override fun onActivityResult

--- app/src/main/java/com/example/doctors/autorization/data/signIn/SignInFragment.kt
+++ app/src/main/java/com/example/doctors/autorization/data/signIn/SignInFragment.kt
@@ -1,17 +1,20 @@
 package com.example.doctors.autorization.data.signIn
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.doctors.R
 import com.example.doctors.RC_SIGN_IN
 import com.example.doctors.databinding.FragmentSignInBinding
 import com.example.doctors.databinding.FragmentSplashBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.material.snackbar.Snackbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignInFragment : Fragment() {
@@ -30,6 +33,15 @@ class SignInFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            viewModel.logInWithGoogle()
+            Snackbar.make(binding.root, "yes", Snackbar.LENGTH_LONG).show()
+        }
     }
 
     private fun showGoogleAuthorizationActivity() {
