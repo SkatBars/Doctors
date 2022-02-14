c14c054a0827b5b52b6b875a3e8e447f3c31d795
1_skat_1 <skat.apps@gmail.com>
2022-02-14 23:18:11 +0300

    create data class Doctor

---
+++ app/src/main/java/com/example/doctors/main/doctorsAppointment/data/Doctor.kt
@@ -0,0 +1,8 @@
+package com.example.doctors.main.doctorsAppointment.data
+
+data class Doctor(
+    val avaragePrice: Int,
+    val name: String,
+    val rating: Double,
+    val urlAvatar: String
+)
