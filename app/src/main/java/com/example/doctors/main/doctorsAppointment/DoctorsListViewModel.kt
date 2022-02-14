986ce53ca59c5a3933dfc280a04257f6d1d5b68e
1_skat_1 <skat.apps@gmail.com>
2022-02-15 00:18:29 +0300

    add fun signOut in DoctorsListViewModel.kt

--- app/src/main/java/com/example/doctors/main/doctorsAppointment/DoctorsListViewModel.kt
+++ app/src/main/java/com/example/doctors/main/doctorsAppointment/DoctorsListViewModel.kt
@@ -1,11 +1,27 @@
 package com.example.doctors.main.doctorsAppointment
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.doctors.authorization.data.FirebaseAuthDataSource
 import com.example.doctors.main.doctorsAppointment.data.DoctorsRecordRemoteDataSource
+import kotlinx.coroutines.launch
 
 class DoctorsListViewModel(
     private val myAuth : FirebaseAuthDataSource,
     private val db: DoctorsRecordRemoteDataSource
     ) : ViewModel() {
+
+    private val _openSignInFragment = MutableLiveData<Boolean>(false)
+    val openSignInFragment: LiveData<Boolean>
+        get() = _openSignInFragment
+
+    fun signOut() {
+        viewModelScope.launch {
+            myAuth.signOut()
+            _openSignInFragment.value = true
+        }
+    }
 }
