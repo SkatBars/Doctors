7c17e6f39e134f830b472cd072a3e5b5f855fb0a
1_skat_1 <skat.apps@gmail.com>
2022-02-16 17:09:21 +0300

    replace ImageView to RoundedImageView

--- app/src/main/java/com/example/doctors/util/BindingUtil.kt
+++ app/src/main/java/com/example/doctors/util/BindingUtil.kt
@@ -4,9 +4,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.github.siyamed.shapeimageview.RoundedImageView
 
 @BindingAdapter("loadImage")
-fun loadImage(view: ImageView, url: String) {
+fun loadImage(view: RoundedImageView, url: String) {
     Glide.with(view.context)
         .load(url)
         .into(view)
