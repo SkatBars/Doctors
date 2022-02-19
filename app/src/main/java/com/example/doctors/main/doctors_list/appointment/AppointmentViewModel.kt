3dc4a7eb7f3b5412e865bba1e0b68e4d2f53d319
1_skat_1 <skat.apps@gmail.com>
2022-02-19 17:55:12 +0300

    create variable places in AppointmentViewModel.kt

--- app/src/main/java/com/example/doctors/main/doctors_list/appointment/AppointmentViewModel.kt
+++ app/src/main/java/com/example/doctors/main/doctors_list/appointment/AppointmentViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.doctors.main.doctors_list.appointment
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.example.doctors.main.doctors_list.data.DoctorsRecordRemoteDataSource
+import com.example.doctors.main.doctors_list.data.PlaceToWrite
 
 class AppointmentViewModel(private val db: DoctorsRecordRemoteDataSource) : ViewModel() {
-
+    var places: LiveData<List<PlaceToWrite>> = db.places
 }
