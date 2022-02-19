bbf5a01ed54c5bb3b302c1c2668078c8c42fdd52
1_skat_1 <skat.apps@gmail.com>
2022-02-19 15:35:50 +0300

    add funs configureRecyclerView and changeListRecyclerView

--- app/src/main/java/com/example/doctors/main/doctors_list/appointment/AppointmentFragment.kt
+++ app/src/main/java/com/example/doctors/main/doctors_list/appointment/AppointmentFragment.kt
@@ -1,19 +1,20 @@
 package com.example.doctors.main.doctors_list.appointment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.doctors.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.doctors.databinding.AppointmentFragmentBinding
+import com.example.doctors.main.doctors_list.data.PlaceToWrite
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AppointmentFragment : Fragment() {
 
     private val viewModel: AppointmentViewModel by viewModel()
     private lateinit var  binding: AppointmentFragmentBinding
+    private lateinit var adapter: AppointmentAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +22,19 @@ class AppointmentFragment : Fragment() {
     ): View {
         binding = AppointmentFragmentBinding.inflate(inflater)
         return binding.root
+    }
+
+    private fun configureRecyclerView(places: List<PlaceToWrite>) {
+        adapter = AppointmentAdapter(places.toMutableList())
+
+        with(binding.appointmentRecyclerView) {
+            adapter = adapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    private fun changeListRecyclerView(newListWithPlaces: List<PlaceToWrite>) {
+        adapter.changeItems(newListWithPlaces)
     }
 
 
