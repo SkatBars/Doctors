f8ac8c56157cbee69b6cf2b4d745bc0bd34234a6
1_skat_1 <skat.apps@gmail.com>
2022-02-19 21:20:23 +0300

    add logic supplement empty places

--- app/src/main/java/com/example/doctors/Constans.kt
+++ app/src/main/java/com/example/doctors/Constans.kt
@@ -1,3 +1,4 @@
 package com.example.doctors
 
 const val RC_SIGN_IN = 1
+const val COUNT_PLACES_FOR_WRITE_OF_DAY = 8
--- app/src/main/java/com/example/doctors/main/doctors_list/data/DoctorsRecordRemoteDataSource.kt
+++ app/src/main/java/com/example/doctors/main/doctors_list/data/DoctorsRecordRemoteDataSource.kt
@@ -2,6 +2,7 @@ package com.example.doctors.main.doctors_list.data
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.doctors.COUNT_PLACES_FOR_WRITE_OF_DAY
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
@@ -13,8 +14,8 @@ import java.util.*
 
 class DoctorsRecordRemoteDataSource(private val firestore: FirebaseFirestore) {
 
-    private val _places = MutableLiveData<List<PlaceToWrite>>()
-    val places: LiveData<List<PlaceToWrite>>
+    private val _places = MutableLiveData<MutableList<PlaceToWrite>>()
+    val places: LiveData<MutableList<PlaceToWrite>>
         get() = _places
 
     private val dispatcher = Dispatchers.IO
@@ -45,20 +46,50 @@ class DoctorsRecordRemoteDataSource(private val firestore: FirebaseFirestore) {
     }
 
     fun enableListenerCollection(doctor: String, currentDate: Date) {
-        val query = firestore
-            .collection("doctors").document(doctor)
-            .collection("years").document(currentDate.year.toString())
-            .collection("months").document(currentDate.month.toString())
-            .collection("days").document(currentDate.day.toString())
-            .collection("places").orderBy("number")
 
-        snapshotListener = query.addSnapshotListener { value, error ->
-            _places.value = value!!.toObjects(PlaceToWrite::class.java)
+        val tempList = mutableListOf<PlaceToWrite>()
+
+        for (i in 0 until COUNT_PLACES_FOR_WRITE_OF_DAY) {
+            tempList.add(PlaceToWrite(
+                doctor, "", i,
+                getTimeByNumber(i), currentDate, false
+            ))
+        }
+
+        _places.value = tempList
+
+    val query = firestore
+        .collection("doctors").document(doctor)
+        .collection("years").document(currentDate.year.toString())
+        .collection("months").document(currentDate.month.toString())
+        .collection("days").document(currentDate.day.toString())
+        .collection("places").orderBy("number")
+
+    snapshotListener = query.addSnapshotListener{ value, error ->
+        if (value != null) {
+            if (value.size() == COUNT_PLACES_FOR_WRITE_OF_DAY) {
+                _places.value = value.toObjects(PlaceToWrite::class.java)
+            } else {
+                val currentList = value.toObjects(PlaceToWrite::class.java)
+                val tempList = _places.value!!
+
+                for (place in currentList) {
+                    tempList[place.number] = place
+                }
+
+                _places.value = tempList
+            }
         }
     }
+}
 
-    fun disableListenerCollectionPlaces() {
-        snapshotListener.remove()
-    }
+
+private fun getTimeByNumber(number: Int): String {
+    return "${number + 9}:00-${number + 10}:00"
+}
+
+fun disableListenerCollectionPlaces() {
+    snapshotListener.remove()
+}
 
 }
