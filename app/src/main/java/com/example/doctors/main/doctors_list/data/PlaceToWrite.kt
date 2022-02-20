0d7924f704af74016f38dd4a8004019e09b57de5
syoma <skat.apps@gmail.com>
2022-02-20 15:08:46 +0300

    set default values for varriable

--- app/src/main/java/com/example/doctors/main/doctors_list/data/PlaceToWrite.kt
+++ app/src/main/java/com/example/doctors/main/doctors_list/data/PlaceToWrite.kt
@@ -3,11 +3,11 @@ package com.example.doctors.main.doctors_list.data
 import java.util.*
 
 data class PlaceToWrite(
-    val idDoctor: String,
-    val idPatient: String,
-    val number: Int,
-    val time: String,
-    val date: Date,
+    val idDoctor: String = "",
+    val idPatient: String = "",
+    val number: Int = 0,
+    val time: String = "",
+    val date: Date = Date(),
     @field:JvmField
-    val isTaken: Boolean
+    val isTaken: Boolean = false
 )
