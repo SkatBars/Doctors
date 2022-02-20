dcdac3428dcdbe73658d6aee9506e9eca90d6c0e
syoma <skat.apps@gmail.com>
2022-02-20 16:27:47 +0300

    change val to var in PlaceToWrite.kt

--- app/src/main/java/com/example/doctors/main/doctors_list/data/PlaceToWrite.kt
+++ app/src/main/java/com/example/doctors/main/doctors_list/data/PlaceToWrite.kt
@@ -4,10 +4,10 @@ import java.util.*
 
 data class PlaceToWrite(
     val idDoctor: String = "",
-    val idPatient: String = "",
+    var idPatient: String = "",
     val number: Int = 0,
     val time: String = "",
     val date: Date = Date(),
     @field:JvmField
-    val isTaken: Boolean = false
+    var isTaken: Boolean = false
 )
