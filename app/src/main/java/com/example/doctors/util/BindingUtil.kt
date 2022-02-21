7e33fc5440fe6c291dde61216c2abed109ce13aa
syoma <skat.apps@gmail.com>
2022-02-21 15:46:10 +0300

    add convert date to string for binding

--- app/src/main/java/com/example/doctors/util/BindingUtil.kt
+++ app/src/main/java/com/example/doctors/util/BindingUtil.kt
@@ -1,10 +1,16 @@
 package com.example.doctors.util
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingConversion
 import com.bumptech.glide.Glide
 import com.github.siyamed.shapeimageview.RoundedImageView
+import java.lang.String.format
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("loadImage")
 fun loadImage(view: RoundedImageView, url: String) {
@@ -16,5 +22,12 @@ fun loadImage(view: RoundedImageView, url: String) {
 @BindingAdapter("setPrice")
 fun setPrice(view: TextView, price: Int) {
     view.text = "Средний чек: $price руб"
+}
+
+@SuppressLint("SimpleDateFormat")
+@BindingConversion
+fun convertDateToString(date: Date): String {
+    val formatter = SimpleDateFormat("Дата: dd.MM.yyyy")
+    return formatter.format(date)
 }
 
