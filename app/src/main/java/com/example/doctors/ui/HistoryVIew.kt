92b0a5f3c87acaa197b7383a2a7764e123bc8821
syoma <skat.apps@gmail.com>
2022-05-30 15:34:57 +0300

    create colors and theme on compose

---
+++ app/src/main/java/com/example/doctors/ui/HistoryVIew.kt
@@ -0,0 +1,10 @@
+package com.example.doctors.ui
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HistoryView() {
+    Text(text = "History", fontSize = 48.sp)
+}
---
+++ app/src/main/java/com/example/doctors/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.doctors.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue500 = Color(0xFF08B2E3)
+val Blue700 = Color(0xFF056D8A)
+val Red = Color(0xFFEE6352)
+val Gray500 = Color(0xFFEFE9F4)
+val Gray700 = Color(0xFF484D6D)
+val Green = Color(0xFF57A773)
