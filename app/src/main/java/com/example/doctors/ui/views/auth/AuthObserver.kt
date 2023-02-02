8d369e5a01a7d8fb46b77b9cb2cbd419a3268f3c
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-02 15:54:07 +0300

    Переименование файла

--- app/src/main/java/com/example/doctors/ui/views/auth/AuthObserver.kt
+++ app/src/main/java/com/example/doctors/ui/views/auth/AuthObserver.kt
@@ -2,21 +2,12 @@ package com.example.doctors.ui.views.auth
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import com.example.doctors.Screen
-import com.example.doctors.ui.components.TextFieldsWithLabelError
-import com.example.doctors.util.emailIfValid
 import com.example.doctors.view_model.AuthorizationViewModel
 import kotlinx.coroutines.launch
 
