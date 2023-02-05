e7da9cebecf6af566f53be8a8082e45d78f0730e
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-05 22:20:02 +0300

    Первичное оформление профиля

--- app/src/main/java/com/example/doctors/ui/views/main/profile/Profile.kt
+++ app/src/main/java/com/example/doctors/ui/views/main/profile/Profile.kt
@@ -1,75 +1,81 @@
 package com.example.doctors.ui
 
 import android.widget.Toolbar
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.doctors.entities.Toothes
+import com.example.doctors.ui.components.TextWithCaption
+import com.example.doctors.ui.theme.*
 import com.example.doctors.ui.views.main.profile.ChangeCurrentTooth
 import com.example.doctors.ui.views.main.profile.Mouth
+import com.example.doctors.util.parseListIdToListToothes
 import com.example.doctors.view_model.InformationUserViewModel
+
+@Composable
+private fun TitleUser(name: String) {
+    Text(
+        text = name,
+        style = MaterialTheme.typography.h4,
+        color = Blue500,
+        fontWeight = FontWeight.Bold
+    )
+
+    Spacer(
+        modifier = Modifier
+            .width(200.dp)
+            .height(3.dp)
+            .background(Blue200)
+    )
+}
 
 @Composable
 fun Profile() {
     val viewModelUser: InformationUserViewModel = viewModel()
-
-    viewModelUser.getToothes()
-
-    val toothes: List<Toothes> by viewModelUser.toothes.observeAsState(emptyList())
+    val userInfo by viewModelUser.userInfo.observeAsState()
     val indexSelected = remember { mutableStateOf(5) }
 
-    Column {
-        TopAppBar {
-            LogoText(PaddingValues(start = 8.dp))
-        }
+    LaunchedEffect(key1 = Unit, block = {
+        viewModelUser.getUserInformation()
+    })
 
-        Text(
-            text = "Здесь вы можете ознакомиться с состоянием ваших зубов на данный момент",
-            textAlign = TextAlign.Center,
-            fontSize = 18.sp,
+    userInfo?.let { info ->
+
+        Column(
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
-        )
+                .fillMaxSize()
+                .padding(horizontal = 8.dp, vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            TitleUser(info.name)
 
-        if (toothes.isNotEmpty()) {
-            Card(modifier = Modifier.padding(16.dp)) {
-                Column(
-                    modifier = Modifier.padding(8.dp),
-                    verticalArrangement = Arrangement.Bottom
-                ) {
-                    ChangeCurrentTooth(toothes = toothes, indexSelected = indexSelected)
-                    Mouth(toothes = toothes, indexSelected = indexSelected)
-                }
+            TextWithCaption(caption = "email:", text = info.email)
+            TextWithCaption(caption = "Номер телефона:", text = info.phoneNumber)
+            TextWithCaption(caption = "Информация:", text = info.information)
+
+            val toothes = parseListIdToListToothes(info.toothes)
+            Column(
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Mouth(toothes = toothes, indexSelected = indexSelected)
+                ChangeCurrentTooth(toothes = toothes, indexSelected = indexSelected)
             }
         }
     }
 }
 
-@Composable
-fun LogoText(paddingValues: PaddingValues) {
-    Text(
-        text = "Анализ ротовой полости",
-        color = Color.White,
-        fontSize = 24.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier
-            .padding(paddingValues)
-            .fillMaxWidth()
-    )
-}
 
