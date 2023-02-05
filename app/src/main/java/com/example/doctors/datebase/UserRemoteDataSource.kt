805cc782f4ff7f2c2ef5ca80e6bb1639123b6f7b
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-05 20:30:06 +0300

    Рефакторинг: добавление входа в suspend функцию

--- app/src/main/java/com/example/doctors/datebase/UserRemoteDataSource.kt
+++ app/src/main/java/com/example/doctors/datebase/UserRemoteDataSource.kt
@@ -4,16 +4,19 @@ import android.annotation.SuppressLint
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 @SuppressLint("StaticFieldLeak")
 object UserRemoteDataSource {
     private val firestore = FirebaseFirestore.getInstance()
+    private val dispatcher = Dispatchers.IO
 
-    suspend fun getUserInfo(userId: String): Task<DocumentSnapshot> {
-        return firestore.collection("users").document(userId).get()
+    suspend fun getUserInfo(userId: String): Task<DocumentSnapshot> = withContext(dispatcher) {
+        return@withContext firestore.collection("users").document(userId).get()
     }
 
-    suspend fun getHistory(userId: String) {
+    suspend fun getHistory(userId: String) = withContext(dispatcher) {
         firestore.collection("users").document(userId).collection("history").get()
     }
 }
