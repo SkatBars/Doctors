f90a7675ff7db94eb99a3cb07e3d1ae3f24f46c7
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-05 22:19:52 +0300

    Создание компонента

---
+++ app/src/main/java/com/example/doctors/ui/components/TextWithCaption.kt
@@ -0,0 +1,25 @@
+package com.example.doctors.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TextWithCaption(caption: String, text: String, modifier: Modifier = Modifier) {
+    Row(horizontalArrangement = Arrangement.Start) {
+        Text(text = caption, style = MaterialTheme.typography.h5, fontWeight = FontWeight.Bold)
+        Text(
+            text = text,
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Normal,
+            modifier = modifier.padding(start = 8.dp)
+        )
+    }
+}
