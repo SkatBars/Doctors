89373712ad913150d8631a25eb7ffd139fd450dd
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-08 17:01:16 +0300

    Добавление нового поля

--- app/src/main/java/com/example/doctors/entities/History.kt
+++ app/src/main/java/com/example/doctors/entities/History.kt
@@ -5,6 +5,7 @@ import java.util.*
 data class History(
     val id: String = UUID.randomUUID().toString(),
     val date: Date = Date(),
+    val nameDoctor: String,
     val doctorId: String = "",
     val description: String = "",
     val ratingForDoctor: Int = 0,
