9ab406105ad946cd81371b139c28b7f954ee790d
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-08 17:11:09 +0300

    Написание ui вывода истории

---
+++ app/src/main/java/com/example/doctors/ui/views/main/history/HistoryItem.kt
@@ -0,0 +1,47 @@
+package com.example.doctors.ui.views.main.history
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.doctors.DoctorsScreen
+import com.example.doctors.entities.History
+import com.example.doctors.ui.components.CircleImage
+import com.example.doctors.ui.components.RatingText
+import com.example.doctors.ui.components.TextWithCaption
+import com.example.doctors.ui.components.Title
+import java.text.SimpleDateFormat
+
+@Composable
+fun HistoryItem(history: History, updateRating: (newValue: Int) -> Unit, currentRating: Int) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = MaterialTheme.colors.surface,
+    ) {
+        Column(
+            modifier = Modifier.padding(start = 16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Title(text = history.nameDoctor, fontSize = 20.sp)
+
+            TextWithCaption(
+                caption = "Дата",
+                text = SimpleDateFormat("dd.mm.yyyy").format(history.date),
+                fontSize = 16.sp
+            )
+
+            TextWithCaption(
+                caption = "Детали",
+                text = history.description,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/doctors/ui/views/main/history/HistoryVIew.kt
+++ app/src/main/java/com/example/doctors/ui/views/main/history/HistoryVIew.kt
@@ -1,15 +1,68 @@
 package com.example.doctors.ui
 
 import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.doctors.DoctorsScreen
 import com.example.doctors.entities.Doctor
+import com.example.doctors.entities.History
+import com.example.doctors.ui.components.CircleImage
+import com.example.doctors.ui.components.RatingText
+import com.example.doctors.ui.components.TextWithCaption
+import com.example.doctors.ui.components.Title
+import com.example.doctors.ui.views.main.history.HistoryItem
+import com.example.doctors.view_model.DoctorsListViewModel
+import com.example.doctors.view_model.InformationUserViewModel
 
 @Composable
 fun HistoryView() {
-    Log.i("QWE", "history")
-    Text(text = "History", fontSize = 48.sp)
+    val userViewModel = viewModel(InformationUserViewModel::class.java)
+    val doctorViewModel = viewModel(DoctorsListViewModel::class.java)
+
+    val history = userViewModel.history.observeAsState()
+
+    LaunchedEffect(key1 = Unit, block = {
+        userViewModel.getHistory()
+    })
+
+    if (history.value?.isNotEmpty() == true) {
+        LazyColumn(
+            Modifier
+                .fillMaxSize()
+                .padding(horizontal = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(history.value!!) { historyItem ->
+                var currentRating by remember { mutableStateOf(historyItem.ratingForDoctor) }
+
+                HistoryItem(
+                    history = historyItem,
+                    updateRating = {
+                        val isFirstUpdateRating = currentRating == 0
+                        currentRating = it
+                        userViewModel.updateRating(historyItem, currentRating)
+                        doctorViewModel.updateRating(
+                            historyItem.doctorId,
+                            currentRating,
+                            isFirstUpdateRating
+                        )
+                    },
+                    currentRating = currentRating
+                )
+            }
+        }
+    }
 }
+
